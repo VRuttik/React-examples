cf476e3eff575810203f720e4abba34edf3e90d8
Vruttik Moragha <97348689+VRuttik@users.noreply.github.com>
2024-06-20 11:56:40 +0000

    main

--- practice-projects/src/components/Product.js
+++ practice-projects/src/components/Product.js
@@ -4,10 +4,9 @@ import React from 'react'
 function Product(props) {
     return (
       <div>
-{/*it uses the value of props by defining the parameter as props objects8 */}
         <img src={props.img} alt="products" />
         <h4>{props.name}</h4>
-        <p>{props.description}</p>
+        <p>{props.desc}</p>
         <h4>{props.price}</h4>
       </div>
     );
